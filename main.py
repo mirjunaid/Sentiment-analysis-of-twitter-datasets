16e1362a17a04d3a2cf3ae48066737a8f9363e56
Junaid Ahmad Mir <79140912+mirjunaid@users.noreply.github.com>
2023-08-23 22:05:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,317 @@
 
+# utilities
+import re
+import numpy as np
+import pandas as pd
+# plotting
+import seaborn as sns
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+# nltk
+from nltk.stem import WordNetLemmatizer
+# sklearn
+from sklearn.svm import LinearSVC
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import confusion_matrix, classification_report
+# Importing the dataset
+DATASET_COLUMNS=['target','ids','date','flag','user','text']
+DATASET_ENCODING = "ISO-8859-1"
+df = pd.read_csv('Project_Data.csv', encoding=DATASET_ENCODING, names=DATASET_COLUMNS)
+df.sample(5)
+
+# Five top records of data
+
+df.head()
+
+
+df.columns
+
+print('length of data is', len(df))
+
+df. shape
+
+df.info()
+
+df.dtypes
+
+np.sum(df.isnull().any(axis=1))
+
+print('Count of columns in the data is:  ', len(df.columns))
+print('Count of rows in the data is:  ', len(df))
+
+df['target'].unique()
+
+df['target'].nunique()
+
+# Plotting the distribution for dataset.
+ax = df.groupby('target').count().plot(kind='bar', title='Distribution of data',legend=False)
+ax.set_xticklabels(['Negative','Positive'], rotation=0)
+# Storing data in lists.
+text, sentiment = list(df['text']), list(df['target'])
+
+class imbalance twitter sentiment analysis
+import seaborn as sns
+sns.countplot(x='target', data=df)
+
+# Data PrePreprocessinPrepr the above-given problem statement, before training the model, we performed various pre-processing steps on the dataset that mainly dealt with removing stopwords, removing special characters like emojis, hashtags, etc. The text document is then converted into lowercase for better generalization.
+
+#Subsequently, the punctuations were cleaned and removed, thereby reducing the unnecessary noise from the dataset. After that, we also removed the repeating characters from the words along with removing the URLs as they do not have any significant importance.
+
+At last, we then performed Stemming(reducing the words to their derived stems) and Lemmatization(reducing the derived words to their root form, known as lemma) for better results.
+
+
+data=df[['text','target']]
+
+data['target'] = data['target'].replace(4,1)
+# Printing unique values of target variables
+
+data['target'].unique()
+# Separating positive and negative tweets
+
+data_pos = data[data['target'] == 1]
+data_neg = data[data['target'] == 0]
+# Taking one-fourth of the data so we can run it on our machine easily
+
+data_pos = data_pos.iloc[:int(20000)]
+data_neg = data_neg.iloc[:int(20000)]
+# Combining positive and negative tweets
+
+dataset = pd.concat([data_pos, data_neg])
+# Making statement text in lowercase
+
+dataset['text']=dataset['text'].str.lower()
+dataset['text'].tail()
+
+# Defining set containing all stopwords in English.
+
+stopwordlist = ['a', 'about', 'above', 'after', 'again', 'ain', 'all', 'am', 'an',
+             'and','any','are', 'as', 'at', 'be', 'because', 'been', 'before',
+             'being', 'below', 'between','both', 'by', 'can', 'd', 'did', 'do',
+             'does', 'doing', 'down', 'during', 'each','few', 'for', 'from',
+             'further', 'had', 'has', 'have', 'having', 'he', 'her', 'here',
+             'hers', 'herself', 'him', 'himself', 'his', 'how', 'i', 'if', 'in',
+             'into','is', 'it', 'its', 'itself', 'just', 'll', 'm', 'ma',
+             'me', 'more', 'most','my', 'myself', 'now', 'o', 'of', 'on', 'once',
+             'only', 'or', 'other', 'our', 'ours','ourselves', 'out', 'own', 're','s', 'same', 'she', "shes", 'should', "shouldve",'so', 'some', 'such',
+             't', 'than', 'that', "thatll", 'the', 'their', 'theirs', 'them',
+             'themselves', 'then', 'there', 'these', 'they', 'this', 'those',
+             'through', 'to', 'too','under', 'until', 'up', 've', 'very', 'was',
+             'we', 'were', 'what', 'when', 'where','which','while', 'who', 'whom',
+             'why', 'will', 'with', 'won', 'y', 'you', "youd","youll", "youre",
+             "youve", 'your', 'yours', 'yourself', 'yourselves']
+# Cleaning and removing the above stop words list from the tweet text
+
+STOPWORDS = set(stopwordlist)
+def cleaning_stopwords(text):
+    return " ".join([word for word in str(text).split() if word not in STOPWORDS])
+dataset['text'] = dataset['text'].apply(lambda text: cleaning_stopwords(text))
+dataset['text'].head()
+
+# Cleaning and removing punctuations
+
+import string
+english_punctuations = string.punctuation
+punctuations_list = english_punctuations
+def cleaning_punctuations(text):
+    translator = str.maketrans('', '', punctuations_list)
+    return text.translate(translator)
+dataset['text']= dataset['text'].apply(lambda x: cleaning_punctuations(x))
+dataset['text'].tail()
+
+# Cleaning and removing repeating characters
+
+def cleaning_repeating_char(text):
+    return re.sub(r'(.)1+', r'1', text)
+dataset['text'] = dataset['text'].apply(lambda x: cleaning_repeating_char(x))
+dataset['text'].tail()
+
+# Cleaning and removing URLs
+
+def cleaning_URLs(data):
+    return re.sub('((www.[^s]+)|(https?://[^s]+))',' ',data)
+dataset['text'] = dataset['text'].apply(lambda x: cleaning_URLs(x))
+dataset['text'].tail()
+
+# Cleaning and removing numeric numbers
+
+def cleaning_numbers(data):
+    return re.sub('[0-9]+', '', data)
+dataset['text'] = dataset['text'].apply(lambda x: cleaning_numbers(x))
+dataset['text'].tail()
+
+# Getting tokenization of tweet text
+
+from nltk.tokenize import RegexpTokenizer
+tokenizer = RegexpTokenizer(r'w+')
+dataset['text'] = dataset['text'].apply(tokenizer.tokenize)
+dataset['text'].head()
+
+# Applying stemming
+
+import nltk
+st = nltk.PorterStemmer()
+def stemming_on_text(data):
+    text = [st.stem(word) for word in data]
+    return data
+dataset['text']= dataset['text'].apply(lambda x: stemming_on_text(x))
+dataset['text'].head()
+
+# Applying lemmatizer
+
+lm = nltk.WordNetLemmatizer()
+def lemmatizer_on_text(data):
+    text = [lm.lemmatize(word) for word in data]
+    return data
+dataset['text'] = dataset['text'].apply(lambda x: lemmatizer_on_text(x))
+dataset['text'].head()
+
+# Separating input feature and label
+
+X=data.text
+y=data.target
+# Plot a cloud of words for negative tweets
+
+data_neg = data['text'][:800000]
+plt.figure(figsize = (20,20))
+wc = WordCloud(max_words = 1000 , width = 1600 , height = 800,
+               collocations=False).generate(" ".join(data_neg))
+plt.imshow(wc)
+
+# Plot a cloud of words for positive tweets
+
+data_pos = data['text'][800000:]
+wc = WordCloud(max_words = 1000 , width = 1600 , height = 800,
+              collocations=False).generate(" ".join(data_pos))
+plt.figure(figsize = (20,20))
+plt.imshow(wc)
+
+# Splitting Our Data Into Train and Test Subsets
+# Separating the 95% data for training data and 5% for testing data
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.05, random_state =26105111)
+# Transforming the Dataset Using TF-IDF Vectorizer 
+#  Fit the TF-IDF Vectorizer
+
+vectoriser = TfidfVectorizer(ngram_range=(1,2), max_features=500000)
+vectoriser.fit(X_train)
+print('No. of feature_words: ', len(vectoriser.get_feature_names()))
+
+# Transform the data using TF-IDF Vectorizer
+
+X_train = vectoriser.transform(X_train)
+X_test  = vectoriser.transform(X_test)
+#  Function for Model Evaluation
+# After training the model, we then apply the evaluation measures to check how the model is performing. Accordingly, we use the following evaluation parameters to check the performance of the models respectively:
+
+"""Accuracy Score
+Confusion Matrix with Plot
+ROC-AUC Curve"""
+def model_Evaluate(model):
+# Predict values for Test dataset
+y_pred = model.predict(X_test)
+# Print the evaluation metrics for the dataset.
+print(classification_report(y_test, y_pred))
+# Compute and plot the Confusion matrix
+cf_matrix = confusion_matrix(y_test, y_pred)
+categories = ['Negative','Positive']
+group_names = ['True Neg','False Pos', 'False Neg','True Pos']
+group_percentages = ['{0:.2%}'.format(value) for value in cf_matrix.flatten() / np.sum(cf_matrix)]
+labels = [f'{v1}n{v2}' for v1, v2 in zip(group_names,group_percentages)]
+labels = np.asarray(labels).reshape(2,2)
+sns.heatmap(cf_matrix, annot = labels, cmap = 'Blues',fmt = '',
+xticklabels = categories, yticklabels = categories)
+plt.xlabel("Predicted values", fontdict = {'size':14}, labelpad = 10)
+plt.ylabel("Actual values" , fontdict = {'size':14}, labelpad = 10)
+plt.title ("Confusion Matrix", fontdict = {'size':18}, pad = 20)
+# Model Building
+# In the problem statement, we have used three different models respectively :
+
+# Bernoulli Naive Bayes Classifier
+# SVM (Support Vector Machine)
+# Logistic Regression
+# The idea behind choosing these models is that we want to try all the classifiers on the dataset ranging from simple ones to complex models, and then try to find out the one which gives the best performance among them.
+
+#  Model-1
+
+BNBmodel = BernoulliNB()
+BNBmodel.fit(X_train, y_train)
+model_Evaluate(BNBmodel)
+y_pred1 = BNBmodel.predict(X_test)
+
+#  Plot the ROC-AUC Curve for model-1
+
+from sklearn.metrics import roc_curve, auc
+fpr, tpr, thresholds = roc_curve(y_test, y_pred1)
+roc_auc = auc(fpr, tpr)
+plt.figure()
+plt.plot(fpr, tpr, color='darkorange', lw=1, label='ROC curve (area = %0.2f)' % roc_auc)
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('ROC CURVE')
+plt.legend(loc="lower right")
+plt.show()
+
+# Model-2:
+
+SVCmodel = LinearSVC()
+SVCmodel.fit(X_train, y_train)
+model_Evaluate(SVCmodel)
+y_pred2 = SVCmodel.predict(X_test)
+
+# Plot the ROC-AUC Curve for model-2
+
+from sklearn.metrics import roc_curve, auc
+fpr, tpr, thresholds = roc_curve(y_test, y_pred2)
+roc_auc = auc(fpr, tpr)
+plt.figure()
+plt.plot(fpr, tpr, color='darkorange', lw=1, label='ROC curve (area = %0.2f)' % roc_auc)
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('ROC CURVE')
+plt.legend(loc="lower right")
+plt.show()
+
+# Model-3
+
+LRmodel = LogisticRegression(C = 2, max_iter = 1000, n_jobs=-1)
+LRmodel.fit(X_train, y_train)
+model_Evaluate(LRmodel)
+y_pred3 = LRmodel.predict(X_test)
+
+# Plot the ROC-AUC Curve for model-3
+
+from sklearn.metrics import roc_curve, auc
+fpr, tpr, thresholds = roc_curve(y_test, y_pred3)
+roc_auc = auc(fpr, tpr)
+plt.figure()
+plt.plot(fpr, tpr, color='darkorange', lw=1, label='ROC curve (area = %0.2f)' % roc_auc)
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('ROC CURVE')
+plt.legend(loc="lower right")
+plt.show()
+
+# Model Evaluation
+# Upon evaluating all the models, we can conclude the following details i.e.
+
+# Accuracy: As far as the accuracy of the model is concerned, Logistic Regression performs better than SVM, which in turn performs better than Bernoulli Naive Bayes.
+
+"""F1-score: The F1 Scores for class 0 and class 1 are :
+(a) For class 0: Bernoulli Naive Bayes(accuracy = 0.90) < SVM (accuracy =0.91) < Logistic Regression (accuracy = 0.92)
+(b) For class 1: Bernoulli Naive Bayes (accuracy = 0.66) < SVM (accuracy = 0.68) < Logistic Regression (accuracy = 0.69)
+
+AUC Score: All three models have the same ROC-AUC score.
+
+We, therefore, conclude that the Logistic Regression is the best model for the above-given dataset.
+
+In our problem statement, Logistic Regression follows the principle of Occam’s Razor, which defines that for a particular problem statement, if the data has no assumption, then the simplest model works the best. Since our dataset does not have any assumptions and Logistic Regression is a simple model. Therefore, the concept holds true for the above-mentioned dataset."""
+
+© Copyright 2013-2023 Analytics Vidhya.
